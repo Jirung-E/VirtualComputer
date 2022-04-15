594ff39bb35df05f605dda1b1559fe457e52d271
Jirung-E <73751952+Jirung-E@users.noreply.github.com>
2022-04-15 21:11:28 +0900

    UtilsTest

---
+++ src/TestModule/TestCase/UtilsTest/UtilsTest.cpp
@@ -0,0 +1,14 @@
+#include "UtilsTest.hpp"
+
+#include "../../../Utils/Util.hpp"
+
+using namespace TestModule;
+using namespace Utils;
+
+
+void UtilsTest::main() {
+    print("Hello");
+    println("Hi");
+    log("?");
+    errorlog("!");
+}
